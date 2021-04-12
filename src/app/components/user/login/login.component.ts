c6c5258df2c28a9bbcb904876e1942eed3933898
koby5i <bobkoby5i@xdeq.com>
2021-04-12 22:51:55 +0200

    60 Login Component Page

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../../services/user.service';
+import {User} from '../../../model/user';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: User = new User();
+  errorMessage: string | undefined;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login(){
+    this.userService.login(this.user).subscribe(data => {
+      this.router.navigate(['/profile'])
+    }, err => {
+        this.errorMessage = "Username or password is incorrect."
+    });
+  }
 }
